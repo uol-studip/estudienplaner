155757c52545f59269eefbcb240b8bbf27e55507
Rasmus Fuhse <Krassmus@gmail.com>
2012-07-12 13:02:04 +0200

    cleanup and add plugin-datafields-diff

---
+++ migrations/02_add_plugin_datafields.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ *  Copyright (c) 2012  Rasmus Fuhse <fuhse@data-quest.de>
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ */
+
+//see also diffs/
+
+class InitPlugin extends DBMigration
+{
+	function up(){
+		DBManager::get()->exec("
+            ALTER TABLE `datafields`
+            CHANGE `object_type` `object_type` ENUM( 'sem', 'inst', 'user', 'userinstrole', 'usersemdata', 'roleinstdata', 'plugin' ) NULL DEFAULT NULL
+        ");
+	}
+}
